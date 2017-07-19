8a5808cb0c6d270a967366120505586a23b8a9ed
tamsin <tamsin@elidirhealth.co.uk>
2017-07-19 12:05:18 +0100

    custom groups

--- custom.sh
+++ custom.sh
@@ -6,21 +6,22 @@
 
 
 # add groups
-samba-tool group add CSC-ADMIN
-samba-tool group add CSC-MANAGER
-samba-tool group add CSC-AGENT
-samba-tool group add CSC-COMPLIANCE
-
+samba-tool group add SITE_USER
+samba-tool group add SITE_READONLY
+samba-tool group add SITE_TIGER_A1
+samba-tool group add SITE_LEOPARD_A2
+samba-tool group add SITE_LION_A3
+samba-tool group add SITE_CHEETAH_A4
 
 # add users
-samba-tool user add gogo ia4uV1EeKait
-samba-tool user add gogo2 ia4uV1EeKait
-samba-tool user add gogo3 ia4uV1EeKait
-samba-tool user add gogo4 ia4uV1EeKait
-
+samba-tool user add user1 ia4uV1EeKait
+samba-tool user add user2 ia4uV1EeKait
+samba-tool user add user3 ia4uV1EeKait
+samba-tool user add user4 ia4uV1EeKait
 
 # add users to groups
-samba-tool group addmembers CSC-ADMIN gogo
-samba-tool group addmembers CSC-MANAGER gogo2,gogo
-samba-tool group addmembers CSC-AGENT gogo3
-samba-tool group addmembers CSC-COMPLIANCE gogo4
+samba-tool group addmembers SITE_USER user2,user3,user4
+samba-tool group addmembers SITE_READONLY user1
+samba-tool group addmembers SITE_TIGER_A1 user1,user2
+samba-tool group addmembers SITE_LEOPARD_A2 user1,user2,user3
+samba-tool group addmembers SITE_LION_A3 user3
